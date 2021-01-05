0b1b5264c2165569e1d10a7f9b49aeae5eac8c8c
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-01-05 20:52:11 +0900

    Make it send lumonde image from local file

--- src/commands/lumonde.ts
+++ src/commands/lumonde.ts
@@ -7,6 +7,6 @@ export default {
     description: `**使用方法**
 > $lumonde`,
     func: async (_client: Client, message: Message): Promise<void> => {
-        await message.channel.send({ files: ['https://images-na.ssl-images-amazon.com/images/I/81g-0%2BsHOwL._AC_SL1500_.jpg'] });
+        await message.channel.send({ files: ['res/lumonde.jpg'] });
     }
 };
