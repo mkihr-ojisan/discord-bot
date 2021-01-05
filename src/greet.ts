477bdef7b043d01535de213f0175ac8b9991e002
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-01-05 21:47:06 +0900

    Change greet message

--- src/greet.ts
+++ src/greet.ts
@@ -2,6 +2,6 @@ import { Client } from 'discord.js';
 
 export function initGreeter(client: Client): void {
     client.on('guildCreate', guild => {
-        guild.systemChannel?.send(':sunny: **おはようございます！！！！！！**\n`$help`でコマンド一覧を表示できます。');
+        guild.systemChannel?.send(':sunny: __**おはようございます！！！！！！**__\n`$help`でコマンド一覧を表示できます。');
     });
 }
