de8b33e1ad85773ef38e6c5d61d86ac8c53a8ea5
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-01-05 20:52:50 +0900

    Add description for command $lumonde

--- src/commands/lumonde.ts
+++ src/commands/lumonde.ts
@@ -5,7 +5,10 @@ export default {
     isHidden: true,
     shortDescription: 'ルマンドの画像を表示します。',
     description: `**使用方法**
-> $lumonde`,
+> $lumonde
+
+**説明**
+> ルマンドの画像を表示します。意味はない。`,
     func: async (_client: Client, message: Message): Promise<void> => {
         await message.channel.send({ files: ['res/lumonde.jpg'] });
     }
