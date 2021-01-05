99ba2b46f63b8190cf7af3f65109f34a8eebb05e
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-01-05 22:43:33 +0900

    Change behavior of command $お前はエビ

--- src/commands/youareebi.ts
+++ src/commands/youareebi.ts
@@ -2,6 +2,7 @@ import { Client, Message } from 'discord.js';
 
 export default {
     name: 'お前はエビ',
+    aliases: ['お前は🍤', 'お前は🦐'],
     shortDescription: 'このボットがエビであることを主張します。',
     description: `**使用方法**
 > $お前はエビ
@@ -9,6 +10,15 @@ export default {
 **説明**
 > このボットがエビであることを主張します。ボットは常にエビであることを否定します。`,
     func: async (_client: Client, message: Message): Promise<void> => {
-        await message.channel.send('いいえ、私はザリガニ。');
+        let me;
+        const random = Math.random();
+        if (random < 0.99) {
+            me = 'ザリガニ';
+        } else if (random < 0.999) {
+            me = 'ルーベラ';
+        } else {
+            me = '…エビ';
+        }
+        await message.channel.send(`いいえ、私は${me}。`);
     }
 };
