aee47c10711c726304306c29a0dec60863c7a3fe
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-01-15 01:41:10 +0900

    Add invitation link to command $info

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -1,12 +1,13 @@
-import { MessageEmbed } from 'discord.js';
+import { Client, MessageEmbed } from 'discord.js';
 
 export default {
     name: 'info',
     shortDescription: 'このボットの情報を表示します。',
-    func: async (): Promise<MessageEmbed> => {
+    func: async (client: Client): Promise<MessageEmbed> => {
         return new MessageEmbed()
             .setTitle(':robot: 向原おじさんのBot <:lumonde:789424914414829579>')
             .setDescription('向原おじさんが作成したボットです。向原おじさんの自宅で常時稼働しているRaspberry Pi 3 Model B+上で動いています。機能追加やバグ修正のために時々サーバーを再起動するので、コマンドに反応できないことがありますが許してください。')
-            .addField('ソースコード', 'https://github.com/mkihr-ojisan/discord-bot');
+            .addField('ソースコード', 'https://github.com/mkihr-ojisan/discord-bot')
+            .addField('招待リンク', `https://discord.com/api/oauth2/authorize?client_id=${client.user?.id}&permissions=0&scope=bot`);
     }
 };
