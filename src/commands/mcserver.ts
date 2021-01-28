ae01e013a74541ce023a42b0f45616e9cd1472d4
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-01-29 03:53:06 +0900

    Update command $ismcserverrunning

--- src/commands/mcserver.ts
+++ src/commands/mcserver.ts
@@ -1,24 +1,30 @@
 import { MessageEmbed } from 'discord.js';
 import isReachable from 'is-reachable';
 
-const MCSERVER_HOSTNAME_GLOBAL = 'mki.hrl.cl';
-const MCSERVER_HOST = '192.168.0.37';
-const MCSERVER_PORT = '41836';
+const MCSERVER_VANILLA_ADDRESS = '192.168.0.37:41836';
+const MCSERVER_MOD_ADDRESS = '192.168.0.37:41837';
 
 export default {
     name: 'ismcserverrunning',
     aliases: ['mcs'],
     shortDescription: 'マインクラフトサーバーが起動しているかどうか確認します。',
     func: async (): Promise<MessageEmbed> => {
-        const isRunning = await isReachable(`${MCSERVER_HOST}:${MCSERVER_PORT}`, { timeout: 500 });
+        const isVanillaServerRunning = isReachable(MCSERVER_VANILLA_ADDRESS, { timeout: 500 });
+        const isModServerRunning = isReachable(MCSERVER_MOD_ADDRESS, { timeout: 500 });
 
         const outputMessage = new MessageEmbed();
-        if (isRunning) {
-            outputMessage.setDescription(':o: **マインクラフトサーバーは起動しています。**');
+
+        if (await isVanillaServerRunning) {
+            outputMessage.addField('バニラサーバー', ':white_check_mark: 起動しています。');
         } else {
-            outputMessage.setDescription(':x: **マインクラフトサーバーは起動していません。**');
+            outputMessage.addField('バニラサーバー', ':no_entry: 起動していません。');
         }
-        outputMessage.addField('サーバーアドレス', `${MCSERVER_HOSTNAME_GLOBAL}:${MCSERVER_PORT}`);
+
+        if (await isModServerRunning) {
+            outputMessage.addField('MODサーバー', ':white_check_mark: 起動しています。');
+        } else {
+            outputMessage.addField('MODサーバー', ':no_entry: 起動していません。');
+        }
 
         return outputMessage;
     }
