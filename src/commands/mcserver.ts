7e6c903c9dd88c3a80853aa8cdde1ae9e13879a7
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-02-13 06:45:54 +0900

    Show online player names for command $mcs

--- src/commands/mcserver.ts
+++ src/commands/mcserver.ts
@@ -1,8 +1,14 @@
 import { MessageEmbed } from 'discord.js';
 import isReachable from 'is-reachable';
+import mc from 'minecraft-protocol';
+import { promisify } from 'util';
 
-const MCSERVER_VANILLA_ADDRESS = '192.168.0.37:41836';
-const MCSERVER_MOD_ADDRESS = '192.168.0.37:41837';
+const MCSERVER_VANILLA_HOST = '192.168.0.37';
+const MCSERVER_VANILLA_PORT = 41836;
+const MCSERVER_VANILLA_ADDRESS = MCSERVER_VANILLA_HOST + ':' + MCSERVER_VANILLA_PORT;
+const MCSERVER_MOD_HOST = '192.168.0.37';
+const MCSERVER_MOD_PORT = 41837;
+const MCSERVER_MOD_ADDRESS = MCSERVER_MOD_HOST + ':' + MCSERVER_MOD_PORT;
 
 export default {
     name: 'ismcserverrunning',
@@ -15,13 +21,31 @@ export default {
         const outputMessage = new MessageEmbed();
 
         if (await isVanillaServerRunning) {
-            outputMessage.addField('バニラサーバー', ':white_check_mark: 起動しています。');
+            const pingResult = await promisify(mc.ping)({ host: MCSERVER_VANILLA_HOST, port: MCSERVER_VANILLA_PORT });
+            let players = '';
+            if ('players' in pingResult) {
+                if (pingResult.players.online > 0)
+                    players = '\n参加者: ' + pingResult.players.sample.join(', ');
+                else
+                    players = '\n参加者: なし';
+            }
+
+            outputMessage.addField('バニラサーバー', ':white_check_mark: 起動しています。' + players);
         } else {
             outputMessage.addField('バニラサーバー', ':no_entry: 起動していません。');
         }
 
         if (await isModServerRunning) {
-            outputMessage.addField('MODサーバー', ':white_check_mark: 起動しています。');
+            const pingResult = await promisify(mc.ping)({ host: MCSERVER_MOD_HOST, port: MCSERVER_MOD_PORT });
+            let players = '';
+            if ('players' in pingResult) {
+                if (pingResult.players.online > 0)
+                    players = '\n参加者: ' + pingResult.players.sample.map(s => s.name).join(', ');
+                else
+                    players = '\n参加者: なし';
+            }
+
+            outputMessage.addField('MODサーバー', ':white_check_mark: 起動しています。' + players);
         } else {
             outputMessage.addField('MODサーバー', ':no_entry: 起動していません。');
         }
