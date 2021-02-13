b4fbafc9bef2b377c01821fc095257b72052087d
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-02-13 16:53:13 +0900

    Rewrite command $mcs

--- src/commands/mcserver.ts
+++ src/commands/mcserver.ts
@@ -3,53 +3,87 @@ import isReachable from 'is-reachable';
 import mc from 'minecraft-protocol';
 import { promisify } from 'util';
 
-const MCSERVER_VANILLA_HOST = '192.168.0.37';
-const MCSERVER_VANILLA_PORT = 41836;
-const MCSERVER_VANILLA_ADDRESS = MCSERVER_VANILLA_HOST + ':' + MCSERVER_VANILLA_PORT;
-const MCSERVER_MOD_HOST = '192.168.0.37';
-const MCSERVER_MOD_PORT = 41837;
-const MCSERVER_MOD_ADDRESS = MCSERVER_MOD_HOST + ':' + MCSERVER_MOD_PORT;
+interface Server {
+    name: string,
+    host: string,
+    port: number,
+}
+const servers: Server[] = [
+    {
+        name: 'バニラサーバー',
+        host: '192.168.0.37',
+        port: 41836,
+    },
+    {
+        name: 'MODサーバー',
+        host: '192.168.0.37',
+        port: 41837,
+    }
+];
+
+type ServerStatus =
+    { server: Server, isRunning: false; } |
+    { server: Server, isRunning: true; } |
+    { server: Server, isRunning: true, onlinePlayerCount: number; } |
+    { server: Server, isRunning: true, onlinePlayerNames: string[]; };
 
 export default {
     name: 'ismcserverrunning',
     aliases: ['mcs'],
     shortDescription: 'マインクラフトサーバーが起動しているかどうか確認します。',
     func: async (): Promise<MessageEmbed> => {
-        const isVanillaServerRunning = isReachable(MCSERVER_VANILLA_ADDRESS, { timeout: 500 });
-        const isModServerRunning = isReachable(MCSERVER_MOD_ADDRESS, { timeout: 500 });
-
         const outputMessage = new MessageEmbed();
 
-        if (await isVanillaServerRunning) {
-            const pingResult = await promisify(mc.ping)({ host: MCSERVER_VANILLA_HOST, port: MCSERVER_VANILLA_PORT });
-            let players = '';
-            if ('players' in pingResult) {
-                if (pingResult.players.online > 0)
-                    players = '\n参加者: ' + pingResult.players.sample.join(', ');
-                else
-                    players = '\n参加者: なし';
-            }
-
-            outputMessage.addField('バニラサーバー', ':white_check_mark: 起動しています。' + players);
-        } else {
-            outputMessage.addField('バニラサーバー', ':no_entry: 起動していません。');
-        }
-
-        if (await isModServerRunning) {
-            const pingResult = await promisify(mc.ping)({ host: MCSERVER_MOD_HOST, port: MCSERVER_MOD_PORT });
-            let players = '';
-            if ('players' in pingResult) {
-                if (pingResult.players.online > 0)
-                    players = '\n参加者: ' + pingResult.players.sample.map(s => s.name).join(', ');
-                else
-                    players = '\n参加者: なし';
-            }
-
-            outputMessage.addField('MODサーバー', ':white_check_mark: 起動しています。' + players);
-        } else {
-            outputMessage.addField('MODサーバー', ':no_entry: 起動していません。');
-        }
+        const statuses = await Promise.all(servers.map(getServerStatus));
+        outputMessage.addFields(statuses.map(status => {
+            return {
+                name: status.server.name,
+                value: serverStatusToMessageString(status),
+            };
+        }));
 
         return outputMessage;
     }
 };
+
+async function getServerStatus(server: Server): Promise<ServerStatus> {
+    const isRunning = await isReachable(server.host + ':' + server.port, { timeout: 500 });
+    if (isRunning) {
+        const pingResult = await promisify(mc.ping)({ host: server.host, port: server.port });
+        if ('players' in pingResult) {
+            if (pingResult.players.sample) {
+                return {
+                    isRunning: true,
+                    onlinePlayerNames: pingResult.players.sample.map(s => s.name),
+                    server,
+                };
+            } else {
+                return {
+                    isRunning: true,
+                    onlinePlayerCount: pingResult.players.online,
+                    server,
+                };
+            }
+        } else {
+            return { isRunning: true, server };
+        }
+    } else {
+        return { isRunning: false, server };
+    }
+}
+
+function serverStatusToMessageString(status: ServerStatus): string {
+    let message;
+    if (status.isRunning) {
+        message = ':white_check_mark: 起動しています。';
+        if ('onlinePlayerCount' in status) {
+            message += '\nプレイヤー数: ' + status.onlinePlayerCount;
+        } else if ('onlinePlayerNames' in status) {
+            message += '\nプレイヤー: ' + status.onlinePlayerNames.join(', ');
+        }
+    } else {
+        message = ':no_entry: 起動していません。';
+    }
+
+    return message;
+}
